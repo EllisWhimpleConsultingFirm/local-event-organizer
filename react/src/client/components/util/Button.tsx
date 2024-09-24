8bca0c767a6cd089ac56684f435153d483cf63e2
connorellis <connor.ellis@getredo.com>
2024-09-23 19:33:59 -0600

    css changes for leo

--- react/src/client/components/util/Button.tsx
+++ react/src/client/components/util/Button.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 
 interface ButtonProps {
-    icon?: SVGElement | null;
+    icon?: string | null;
+    icon_description?: string | null;
     text?: string | null;
     style?: string | null;
+    textStyles?: string | null;
     onClick: (event?: React.FormEvent | undefined) => void;
 }
 
-const Button: React.FC<ButtonProps> = ({icon = null, text = null, onClick, style = ""}) => {
+const Button: React.FC<ButtonProps> = ({icon = null, icon_description = null, text = null, onClick, style = "", textStyles = ""}) => {
 
     const styles = `btn flex items-center py-1 px-4 hover:outline-none ${style}`;
 
@@ -24,11 +26,11 @@ const Button: React.FC<ButtonProps> = ({icon = null, text = null, onClick, style
         >
             {icon && (
                 <span className="h-6 w-6">
-                    <img src={icon}/>
+                    <img src={icon} alt={icon_description ?? ''}/>
                 </span>
             )}
             {text && (
-                <span className="p-2">
+                <span className={`p-2 ${textStyles ? textStyles : ''}`}>{text}
                 {text}
             </span>
             )}
