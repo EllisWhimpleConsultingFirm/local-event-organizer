ccb536bbb88d7ca65cfbd42ae394aaa62d1024a6
garett_whimple <garett.whimple@gmail.com>
2024-10-13 18:00:41 -0600

    feat: added api route for events

--- leo/src/app/api/events/route.ts
+++ leo/src/app/api/events/route.ts
@@ -7,7 +7,6 @@ export async function GET() {
     const daoFactory: DAOFactory = new SupabaseDAOFactory();
     const eventsDao = daoFactory.getEventsDAO();
     const eventService = new EventService(eventsDao);
-
     const events = await eventService.getAllEvents();
 
     return NextResponse.json(events);
