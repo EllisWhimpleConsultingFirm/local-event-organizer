2be4aa3c134fad7cd406a1b189b19efececf3a0d
Connor Ellis <ellisconnor88@gmail.com>
2024-11-11 20:54:31 -0700

    pan on map click

--- leo/src/components/util/maps/EventMapClient.tsx
+++ leo/src/components/util/maps/EventMapClient.tsx
@@ -1,22 +1,31 @@
 'use client';
 
+import { useRef } from "react";
 import React from 'react';
 import {GoogleMap, LoadScript, Marker} from "@react-google-maps/api";
 
-type MapOption = { id: string | number; latitude: number | null; longitude: number | null }
+type MapOption = { id: string | number; latitude: number; longitude: number }
 
 type EventMapClientProps<T extends MapOption> = {
     markers: T[];
     addresses: { [key: string]: string };
     apiKey: string;
-    onMarkerClicked?: () => void
 };
 
+export const EventMapClient = <T extends MapOption>({ markers, addresses, apiKey }: EventMapClientProps<T>) => {
+    const mapRef = useRef<google.maps.Map | null>(null);
 
-export const EventMapClient = <T extends MapOption>({ markers, addresses, apiKey, onMarkerClicked }: EventMapClientProps<T>) => {
     const mapCenter = {
         lat: markers[0]?.latitude || 0,
         lng: markers[0]?.longitude || 0,
+    };
+
+    const handleMarkerClick = (event: T) => {
+        if (mapRef.current) {
+            mapRef.current.panTo({ lat: event.latitude, lng: event.longitude });
+            mapRef.current.setZoom(15);
+
+        }
     };
 
     return (
@@ -24,18 +33,19 @@ export const EventMapClient = <T extends MapOption>({ markers, addresses, apiKey
             <div className="w-full h-[350px] rounded-xl overflow-hidden shadow-lg">
                 <LoadScript googleMapsApiKey={apiKey}>
                     <GoogleMap
-                        mapContainerStyle={{width: '100%', height: '100%'}}
+                        mapContainerStyle={{ width: '100%', height: '100%' }}
                         center={mapCenter}
                         zoom={12}
-                        options={{streetViewControl: false}}
+                        options={{ streetViewControl: false }}
+                        onLoad={(map) => (mapRef.current = map)}
                     >
                         {markers.map((event) => (
                             event.latitude !== null && event.longitude !== null ? (
                                 <Marker
-                                    onClick={onMarkerClicked ? () => onMarkerClicked(): () => console.log("clicked")}
                                     key={event.id}
                                     position={{ lat: event.latitude, lng: event.longitude }}
                                     title={addresses[event.id] || 'Fetching address...'}
+                                    onClick={() => handleMarkerClick(event)}
                                 />
                             ) : null
                         ))}
@@ -45,4 +55,3 @@ export const EventMapClient = <T extends MapOption>({ markers, addresses, apiKey
         </div>
     );
 };
-
